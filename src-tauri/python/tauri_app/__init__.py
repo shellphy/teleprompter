9a21b496feaebe86d2327f63a0113d27be90dd33
shellphy <ai.shellphy@gmail.com>
2025-06-05 17:54:25 +0800

    添加python ipc

--- src-tauri/python/tauri_app/__init__.py
+++ src-tauri/python/tauri_app/__init__.py
@@ -1,19 +1,39 @@
-"""The tauri-app."""
+import sys
 
+from anyio.from_thread import start_blocking_portal
+from pydantic import BaseModel
 from pytauri import (
     BuilderArgs,
+    Commands,
     builder_factory,
     context_factory,
 )
 
+commands: Commands = Commands()
+
+
+class Person(BaseModel):
+    name: str
+
+
+class Greeting(BaseModel):
+    message: str
+
+@commands.command()
+async def greet(body: Person) -> Greeting:
+    return Greeting(
+        message=f"Hello, {body.name}! You've been greeted from Python {sys.version}!"
+    )
 
 def main() -> int:
-    """Run the tauri-app."""
-    app = builder_factory().build(
-        BuilderArgs(
-            context=context_factory(),
-            invoke_handler=None,  # TODO
+    with start_blocking_portal("asyncio") as portal:  # or `trio`
+        app = builder_factory().build(
+            BuilderArgs(
+                context=context_factory(),
+                # 👇
+                invoke_handler=commands.generate_handler(portal),
+                # 👆
+            )
         )
-    )
-    exit_code = app.run_return()
-    return exit_code
+        exit_code = app.run_return()
+        return exit_code
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { invoke } from "@tauri-apps/api/core";
+import { pyInvoke } from "tauri-plugin-pytauri-api";
 
 createApp(App).mount("#app");
+
+interface Greeting {
+    message: string;
+  }
+
+async function greet() {
+    const rsGreeting = await invoke<string>("greet", {
+        name: "shellphy",
+    });
+    const pyGreeting = await pyInvoke<Greeting>("greet", {
+        name: "shellphy",
+    });
+    console.log(rsGreeting + "\n" + pyGreeting.message);
+}
+
+greet();
